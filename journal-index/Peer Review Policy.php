b7ff11df470810fb432e1f7601a094186026bf23
mhvbyrne <byrnem@nsamr.ac.uk>
2017-04-14 20:41:36 +0100

    Peer review changes
    Done

--- journal-index/Peer Review Policy.php
+++ journal-index/Peer Review Policy.php
@@ -4,19 +4,22 @@
         <div class="left-heading">Peer Review Process</div>
         <ol>
 			<li>Article submitted.</li>
-			<li>Editor-in-chief/Deputy editor evaluates whether article can be published.</li>
-			<li>Non-publishable articles are sent to JNSAMR advisory board member for review with recommendation to reject.</li>
+			<li>Editor-in-Chief/Deputy Editor evaluates whether article can be published.</li>
 			<li>Publishable articles are sent to the appropriate Section Editor.</li>
-			<li>The Section Editor sends the unblinded article to two or three student reviewers.</li>
+			<li>The Section Editor sends the blinded article to three Peer Reviewers.</li>
 			<li>Reviewers have 48 hours to respond, otherwise an alternative reviewer is selected.</li>
 			<li>Reviewers have 2 weeks to complete their peer review.</li>
-			<li>The Section Editor then reviews the article and comments to compile a peer review summary and letter to the author.</li>
-			<li>The section editor sends the peer review summary and author letter to the Editor-in-Chief with a recommendation to ‘accept with no revisions’, ‘accept with minor revisions’, ‘revise and resubmit’, or ‘reject with no resubmission’.</li>
-			<li>The Editor-in-Chief responds to the author with the letter, peer-review summary, and final decision.</li>
+			<li>The Section Editor then compiles a peer review summary and decision to the author.</li>
+			<li>The Section Editor may choose to ‘accept with no revisions’, ‘accept with minor revisions’, ‘revise and resubmit’, or ‘reject with no resubmission’.</li>
+			<li>If the Section Editor chooses ‘accept with minor revisions’ or ‘revise and resubmit’, the Section Editor should send the peer-review summary and deicision to the Author</li>
 			<li>JNSAMR aims to respond to authors within one month of article submission.</li>
 			<li>If revisions are required the author then has one month to make the suggested changes and resubmit the article.</li>
 			<li>Articles that receive the editorial decision of ‘accept with minor revisions’ are reviewed by original reviewers.</li>
-			<li>Articles that receive the editorial decision of ‘revise and resubmit’ re-enter the normal peer-review process.</li>
+			<li>Articles that receive the editorial decision of ‘revise and resubmit’ are for major revisions and thus must re-enter the normal peer-review process.</li>
+			<li>The submission may need to go through the peer review process multiple times, in which case each time revisions have been made peer reviewers have two weeks to respond and should address whether their suggested changes have been made.</li>
+			<li>These should be summarised to the Section Editor as a complete repeat review is not necessary.</li>
+			<li>Once/If the Section Editor chooses 'accept with no revisions' or ‘reject with no resubmission’, the Section Editor sends the peer-review summary, and decision to the Editor-in-Chief.</li>
+			<li>The Editor-in-Chief reviews the summary and decision and responds to the author with the peer-review summary, and decision to accept.</li>
 		</ol>
     </div>                       
 </div>
@@ -26,7 +29,7 @@
     <div class="col-sm-12">
         <div class="left-heading">Peer Reviewers</div>
         <div>
-			<p>To ensure that prompt, high-quality reviews are sent to authors the following information will be stored for all JNSAMR peer reviewers:</p>
+			<p>To ensure that prompt, high-quality reviews are sent to authors the information about Peer Reviewers will be stored for all JNSAMR peer reviewers, including but not limited to:</p>
 				<ul>
 					<li>Peer reviewer details: name, university, year, email, area of interest.</li>
 					<li>Accepted reviews</li>
@@ -37,8 +40,7 @@
 			<p>Peer reviewers are required to have completed reviewer training and registration (HYPERLINKS XX), and must adhere to all JNSAMR policies (HYPERLINKS XX).</p>	
 			<p>Reviews must be accepted or declined within 48 hours and completed within two weeks.Peer reviewers are required to have completed reviewer training and registration (HYPERLINKS XX), and must adhere to all JNSAMR policies (HYPERLINKS XX).</p>	
 			<p>Peer reviews should maintain communication with Section Editors</p>	
-			<p>Any problems must be communicated promptly with the Section Editor, or if appropriate Deputy Editors.</p>	
-			<p>Only peer reviewers with more than one review with an average rating greater than 4 will be listed in JNSAMR.</p>
+			<p>Any problems must be communicated promptly with the Section Editor, or if appropriate Deputy Editors or Editor-in-Chief.</p>	
     </div>                       
 </div>
 <hr>
@@ -57,26 +59,6 @@
 
 <div class="row">
     <div class="col-sm-12">
-        <div class="left-heading">Peer Reviewer Rating</div>
-        <div>
-			<p>Peer reviewers will be rated by the appropriate section editor after each completed review.</p>
-			<p>Reviewers are assessed on communication, timeliness, and review content.</p>
-			<p>The rating scale is from 0 to 5.</p>
-				<ul>
-					<li>For a rating of 5, reviewers must ensure prompt communication with the Section Editor. The review will be accepted within 48 hours, and completed within two weeks. The review will adhere to JNSAMR peer review guidelines, and will be written in a concise well-written style that requires little revision from Section Editors.</li>
-					<li>For a rating of 4, reviewers must ensure prompt communication with the Section Editor. The review will be accepted within 48 hours, and completed within two weeks. The review will adhere to JNSAMR peer review guidelines, and will be written in a concise well-written style that may require some revision from Section Editors.</li>
-					<li>For a rating of 3, reviewers must ensure prompt communication with the Section Editor. The review will be accepted within 48 hours, and completed within two weeks. The review will adhere to JNSAMR peer review guidelines, but requires revision from Section Editors.</li>
-					<li>For a rating of 2, reviewer communication is poor. The review is completed within two weeks. The review may not adhere to JNSAMR peer review guidelines, or requires substantial revision from Section Editors.</li>
-					<li>For a rating of 1, reviewer communication is poor or review incomplete. The review is completed within 3 weeks.</li>
-					<li>For a rating of 0, reviewer communication is poor. The review is not completed within the maximum of 3 weeks.</li>
-				</ul>
-			<p>Any non-professional behaviour will be automatically awarded a rating of 0, with review by the Editor-in-Chief.</p>
-    </div>                       
-</div>
-<hr>
-
-<div class="row">
-    <div class="col-sm-12">
         <div class="left-heading">Reviewer Dismissal</div>
         <div>
 			<p>Reviewers may be dismissed for any of the following:</p>
@@ -84,7 +66,6 @@
 					<li>Behaviour deemed non-professional by the Section Editors, Deputy Editors or Editor-in-Chief.</li>
 					<li>Complaints from Section Editors, Deputy Editors or Editor-in-Chief.</li>
 					<li>Violation of the Review Ethics Policy (HYPERLINKS XX).</li>
-					<li>Two section Section Editor ratings of 1 or 2.</li>
 				</ol>
 			<p>Reviewer dismissal will be determined by a majority vote from the Deputy Editors and Editor-in-Chief.</p>
 			<p>Dismissal will be recorded in the reviewer database and any dismissed peer reviewers will be unable to reapply to be peer-reviewers.</p>
